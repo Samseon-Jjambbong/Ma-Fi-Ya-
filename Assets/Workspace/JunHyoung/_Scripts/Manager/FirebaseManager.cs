840071d6dcfd040d2de8036c0c086a7ebad05ec4
Park JunHyoung <pj93251428@gmail.com>
2024-05-09 11:49:53 +0900

    feat : FirebaseManager

---
+++ Assets/Workspace/JunHyoung/_Scripts/Manager/FirebaseManager.cs
@@ -0,0 +1,74 @@
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour
+{
+    private static FirebaseManager instance;
+    public static FirebaseManager Instance { get { return instance; } }
+
+    private static FirebaseApp app;
+    public static FirebaseApp App { get { return app; } }
+
+    private static FirebaseAuth auth;
+    public static FirebaseAuth Auth { get { return auth; } }
+
+    private static FirebaseDatabase db;
+
+    public static FirebaseDatabase DB { get { return db; } } 
+
+    private static bool isValid;
+    public static bool IsValid { get { return isValid; } }
+
+
+    /******************************************************
+    *                    Init Settings
+    ******************************************************/
+
+    private void Awake()
+    {
+        CreateInstance();
+        CheckDependency();
+    }
+
+    private void CreateInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private async void CheckDependency()
+    {
+        DependencyStatus dependencyStatus = await FirebaseApp.CheckAndFixDependenciesAsync();
+        if (dependencyStatus == DependencyStatus.Available)
+        {
+            // Create and hold a reference to your FirebaseApp,
+            // where app is a Firebase.FirebaseApp property of your application class.
+            app = FirebaseApp.DefaultInstance;
+            auth = FirebaseAuth.DefaultInstance;
+            db= FirebaseDatabase.DefaultInstance;
+
+            // Set a flag here to indicate whether Firebase is ready to use by your app.
+            Debug.Log("Firebase Check and FixDependencies success");
+            isValid = true;
+        }
+        else
+        {
+            // Firebase Unity SDK is not safe to use here.
+            Debug.LogError("Firebase Check and FixDependencies fail");
+            isValid = false;
+
+            app = null;
+            auth = null;
+            db = null;
+        }
+    }
+}

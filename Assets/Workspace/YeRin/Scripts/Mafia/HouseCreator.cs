7aad2bed7439bfc04373ad4ff9c793906dfad1e5
Mellling <154119653+Mellling@users.noreply.github.com>
2024-05-09 11:21:22 +0900

    feat: Create houses to match the number of players, Create player prefabs in front of each house as many as the number of players

---
+++ Assets/Workspace/YeRin/Scripts/Mafia/HouseCreator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// programmer : Yerin
+/// 
+/// For creating player's houses in Mafia game Scene
+/// </summary>
+public class HouseCreator : MonoBehaviour
+{
+    [SerializeField] GameObject housePrefab;
+    [SerializeField] int playerCount;
+
+    [SerializeField] int radius;    // 21이 가장 이상적
+
+    private void Start()
+    {
+        CreateHouses();
+    }
+
+    private void CreateHouses()
+    {
+        int angle = 180 / (playerCount - 1);    // 각 집의 간격의 각도
+
+        int currentAngle = 0;
+        for (int i = 0; i < playerCount; i++) 
+        {
+            Vector3 pos = new Vector3(Mathf.Cos(currentAngle * Mathf.Deg2Rad) * radius, 1.8f, Mathf.Sin(currentAngle * Mathf.Deg2Rad) * radius);
+            Transform house = Instantiate(housePrefab).transform;
+            house.position = pos;
+
+            Quaternion look = Quaternion.LookRotation(pos); // 센터를 바라보도록 rotation 조절
+            house.rotation = look;
+
+            currentAngle += angle;
+        }
+    }
+}

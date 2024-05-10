9928bd6a5a1d092deeb7566d85c87676c1526bdf
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-05-10 09:38:37 +0900

    fix : lobby room alignment

--- Assets/Workspace/TaeHong/PhotonImport/Lobby/Scripts/RoomEntry.cs
+++ Assets/Workspace/TaeHong/PhotonImport/Lobby/Scripts/RoomEntry.cs
@@ -1,5 +1,6 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +8,7 @@ using UnityEngine.UI;
 public class RoomEntry : MonoBehaviour
 {
     [SerializeField] TMP_Text roomName;
+    [SerializeField] TMP_Text gameMode;
     [SerializeField] TMP_Text currentPlayer;
     [SerializeField] Button joinRoomButton;
 
@@ -22,6 +24,7 @@ public class RoomEntry : MonoBehaviour
     {
         roomInfo = info;
         roomName.text = roomInfo.Name;
+        gameMode.text = Enum.GetName(typeof(GameMode),(GameMode) roomInfo.CustomProperties["GameMode"]);
         currentPlayer.text = $"{roomInfo.PlayerCount} / {roomInfo.MaxPlayers}";
         joinRoomButton.interactable = roomInfo.PlayerCount < roomInfo.MaxPlayers;
     }

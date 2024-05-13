326953e02c3a0d948c979501989960d22653104b
Park JunHyoung <pj93251428@gmail.com>
2024-05-13 12:09:43 +0900

    feat : UpdateLeaderBoard
     리더보드 업데이트 기능 추가

--- Assets/Workspace/JunHyoung/_Scripts/Database/LeaderBoard.cs
+++ Assets/Workspace/JunHyoung/_Scripts/Database/LeaderBoard.cs
@@ -1,5 +1,6 @@
 using Firebase.Database;
 using Firebase.Extensions;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,32 +11,35 @@ public class LeaderBoard : MonoBehaviour
 
     [SerializeField] Button activeButton;
 
+    List<UserRank> userRanks = new List<UserRank>(MAXCOUNT);
+
+    const int MAXCOUNT = 10;
     const string ROOTPATH = "UserData";
     //const string DATA = "score";
     const string DATA = "winCount";
 
     void Start()
     {
-        GetScoreBoard();
+        //InitLeaderBoard();
     }
 
     private void OnEnable()
     {
         FirebaseManager.DB.GetReference(ROOTPATH)
            .OrderByChild(DATA)
-           .LimitToFirst(10)
-           .ValueChanged += ScoreBoardChanged; //변경시 호출될 함수를 ValueChanged에 델리게이트체인을 걸어 사용
+           .LimitToFirst(MAXCOUNT)
+           .ValueChanged += UpdateLeaderBoard; //변경시 호출될 함수를 ValueChanged에 델리게이트체인을 걸어 사용
     }
 
     private void OnDisable()
     {
         FirebaseManager.DB.GetReference(ROOTPATH)
            .OrderByChild(DATA)
-           .LimitToFirst(10)
-           .ValueChanged -= ScoreBoardChanged;
+           .LimitToFirst(MAXCOUNT)
+           .ValueChanged -= UpdateLeaderBoard;
     }
 
-    void GetScoreBoard()
+    void InitLeaderBoard()
     {
         FirebaseManager.DB
              .GetReference(ROOTPATH)
@@ -66,14 +70,53 @@ public class LeaderBoard : MonoBehaviour
                      //Debug.Log($"{name} : {score}");
                      var userRank = Instantiate(prefab, contents);
                      userRank.Set(rank, name ,score);
+                     userRanks.Add(userRank);
                      rank--;
                  }
              });
     }
 
-    private void ScoreBoardChanged(object sendor, ValueChangedEventArgs args)
+    private void UpdateLeaderBoard(object sendor, ValueChangedEventArgs args)
     {
-        Debug.Log("DB 변경됨");
+        FirebaseManager.DB
+             .GetReference(ROOTPATH)
+             .OrderByChild(DATA)
+             .GetValueAsync()
+             .ContinueWithOnMainThread(task =>
+             {
+                 if ( task.IsFaulted )
+                 {
+                     Debug.Log($"Update LeaderBoard data Faulted : {task.Exception.Message}");
+                     return;
+                 }
+                 else if ( task.IsCanceled )
+                 {
+                     Debug.Log("Update LeaderBoard data Canceled");
+                     return;
+                 }
+
+                 DataSnapshot snapshot = task.Result;
+
+                 foreach ( Transform child in contents )
+                 {
+                     Destroy(child.gameObject);  //삭제 생성 대신 더 좋은 방안 생각해 개선할것...
+                                                 // => 풀링 해둔 뒤 disable 하고 enable 하면서 세팅?
+                 }
+
+                 int rank = ( int ) snapshot.ChildrenCount;
+                 Debug.Log(rank);
+                 foreach ( var item in snapshot.Children )
+                 {
+                     string json = item.GetRawJsonValue();
+                     UserData data = JsonUtility.FromJson<UserData>(json);
+                     string name = data.Name;
+                     int score = data.winCount;
+                     var userRank = Instantiate(prefab, contents);
+                     
+                     userRank.Set(rank, name, score);
+                     rank--;
+                 }
+             });
     }
 
 }

1b840ce7a860f0ec1093f55be74b2e080fddbc8a
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-05-13 11:29:46 +0900

    chore : minor stuff

--- Assets/Workspace/TaeHong/Scripts/DebugGameManager.cs
+++ Assets/Workspace/TaeHong/Scripts/DebugGameManager.cs
@@ -13,23 +13,18 @@ namespace Tae
     {
         [SerializeField] private string debugRoomName;
         [SerializeField] int radius;    // 21이 가장 이상적
-
+        
         private List<House> houses;
+        [SerializeField] private int playerCount = 4;
         
         private void Start()
         {
             PhotonNetwork.LocalPlayer.NickName = $"TestPlayer {Random.Range(1000, 10000)}";
             PhotonNetwork.ConnectUsingSettings();
         }
-
-        private void AssignRoles()
-        {
-            
-        }
         
         private void SpawnHouses()
         {
-            int playerCount = 4;
             houses = new List<House>();
         
             int angle = 180 / (playerCount - 1);    // 각 집의 간격의 각도
@@ -43,6 +38,21 @@ namespace Tae
                 currentAngle += angle;
             
                 houses.Add(houseGO.GetComponent<House>());
+            }
+        }
+
+        private void SpawnPlayers()
+        {
+            int angle = 180 / ( playerCount - 1 );    // 각 플레이어의 간격의 각도
+            for ( int i = 0; i < playerCount; i++ )
+            {
+                int currentAngle = 180 - angle * i;
+
+                // 순번에 맞는 플레이어의 위치 설정
+                Vector3 pos = new Vector3(Mathf.Cos(currentAngle * Mathf.Deg2Rad) * (radius - 6), 2.22f,
+                    Mathf.Sin(currentAngle * Mathf.Deg2Rad) * radius);
+                // PhotonNetwork.Instantiate를 통해 각 플레이어 캐릭터 생성, 센터를 바라보도록 rotation 설정
+                Transform player = PhotonNetwork.Instantiate("TestPlayer", pos, Quaternion.LookRotation(pos)).transform;
             }
         }
         
@@ -75,16 +85,20 @@ namespace Tae
             {
                 RandomizeRoles(); // Decide which roles each player shoudl
                 SpawnHouses(); // Spawn {PlayerCount} Houses
+                //SpawnPlayers();
             }
-            
+        }
+
+        private void SpawnPlayer()
+        {
             // Spawn Player in front of respective house
             Debug.Log(PhotonNetwork.LocalPlayer.ActorNumber);
             
-            House house = houses[PhotonNetwork.LocalPlayer.ActorNumber - 1]; // TODO: Fix this
-            Vector3 spawnPos = house.transform.position + house.transform.forward * -5;
-            Quaternion spawnRot = Quaternion.LookRotation(spawnPos);
-            
-            PhotonNetwork.Instantiate("Player", spawnPos, spawnRot, 0);
+            // House house = houses[PhotonNetwork.LocalPlayer.ActorNumber - 1]; // TODO: Fix this
+            // Vector3 spawnPos = house.transform.position + house.transform.forward * -5;
+            // Quaternion spawnRot = Quaternion.LookRotation(spawnPos);
+            //
+            // PhotonNetwork.Instantiate("Player", spawnPos, spawnRot, 0);
         }
     }
 }

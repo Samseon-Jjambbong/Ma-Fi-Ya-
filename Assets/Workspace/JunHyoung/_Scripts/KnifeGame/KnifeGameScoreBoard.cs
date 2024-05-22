64f4c04eb2a7c010576f3a0070d0fb1ddff10e26
Park JunHyoung <pj93251428@gmail.com>
2024-05-22 11:05:40 +0900

    feat : Init ScoreBoard And Update it by Property changes
     todo : 순위에 따라 정렬하는거...

--- Assets/Workspace/JunHyoung/_Scripts/KnifeGame/KnifeGameScoreBoard.cs
+++ Assets/Workspace/JunHyoung/_Scripts/KnifeGame/KnifeGameScoreBoard.cs
@@ -1,24 +1,44 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using PhotonHashtable = ExitGames.Client.Photon.Hashtable;
 public class KnifeGameScoreBoard : MonoBehaviourPunCallbacks
 {
     [SerializeField] RectTransform content;
-    [SerializeField] UserKillDeathEntry entry;
+    [SerializeField] UserKillDeathEntry entryPrefab;
 
+    // KnifeGameManager.Instance.PlayerDic;
+    Dictionary<int, Player> playerDic;
+    Dictionary<int, UserKillDeathEntry> entryDic;
+    UserKillDeathEntry changedEntry;
 
-
-    void Start()
+    public override void OnEnable()
     {
+        base.OnEnable();
+     
+        InitScoreBoard();
+    }
 
+    void InitScoreBoard()
+    {
+        playerDic = KnifeGameManager.Instance.PlayerDic;
+
+        foreach (var player in playerDic.Values)
+        {
+            UserKillDeathEntry entry = Instantiate(entryPrefab, content);
+            entry.Set(player.NickName, player.GetPlayerKillCount(), player.GetPlayerDeathCount());
+            entryDic.Add(player.ActorNumber, entry);
+        }
     }
 
     public override void OnPlayerPropertiesUpdate(Player targetPlayer, PhotonHashtable changedProps)
     {
-        //if (changedProps.ContainsKey(CustomProperty))
+        if (changedProps.ContainsKey(CustomPropertyExtensions.KILL) || changedProps.ContainsKey(CustomPropertyExtensions.DEATH))
         {
-          
+            changedEntry = entryDic[targetPlayer.ActorNumber];
+            changedEntry.UpdateEntry(targetPlayer.GetPlayerKillCount(), targetPlayer.GetPlayerDeathCount());
         }
     }
 }

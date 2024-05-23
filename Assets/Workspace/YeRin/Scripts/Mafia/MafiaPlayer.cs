10d8615c713c28fd1a6dea64c0d0cda314e560a2
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-05-23 11:24:22 +0900

    fix : insane can't get mafia role

--- Assets/Workspace/YeRin/Scripts/Mafia/MafiaPlayer.cs
+++ Assets/Workspace/YeRin/Scripts/Mafia/MafiaPlayer.cs
@@ -66,7 +66,7 @@ public class MafiaPlayer : MonoBehaviourPun
                     actionType = MafiaActionType.Block;
                     break;
                 case MafiaRole.Insane:
-                    actionType = (MafiaActionType) Random.Range(0, 3);
+                    actionType = (MafiaActionType) Random.Range(1, 3);
                     break;
             }
         }

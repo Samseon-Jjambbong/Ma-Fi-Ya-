ad70abb448083f8ef0ad5a8be264af3ca03ee8f8
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-05-23 17:24:37 +0900

    chore : organize workspace

--- Assets/Workspace/TaeHong/Scripts/UI/JobListUI.cs
+++ Assets/Workspace/TaeHong/Scripts/UI/JobListUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Police : MonoBehaviour
+public class JobListUI : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

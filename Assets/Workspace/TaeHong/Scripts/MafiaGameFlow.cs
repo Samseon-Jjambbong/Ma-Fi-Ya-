7000778ff26146e03a858611e2694b697f1f8811
Tae Hong Ahn <33810422+taehong99@users.noreply.github.com>
2024-05-23 15:49:42 +0900

    feat : send win/lose info to db

--- Assets/Workspace/TaeHong/Scripts/MafiaGameFlow.cs
+++ Assets/Workspace/TaeHong/Scripts/MafiaGameFlow.cs
@@ -255,10 +255,12 @@ public class MafiaGameFlow : MonoBehaviourPun
             if (result == MafiaResult.MafiaWin)
             {
                 winLoseUI.ShowWin(100);
+                FirebaseManager.UpdateRecord(100, true);
             }
             else
             {
                 winLoseUI.ShowLose(50);
+                FirebaseManager.UpdateRecord(50);
             }
         }
         // 내가 시민이면
@@ -267,10 +269,12 @@ public class MafiaGameFlow : MonoBehaviourPun
             if (result == MafiaResult.MafiaWin)
             {
                 winLoseUI.ShowLose(50);
+                FirebaseManager.UpdateRecord(50);
             }
             else
             {
                 winLoseUI.ShowWin(100);
+                FirebaseManager.UpdateRecord(100, true);
             }
         }
         
